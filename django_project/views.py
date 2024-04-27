43fdd216d3f29b2d048cf09873781e2f0cd13fcb
Jimi001 <119814048+Jimi001@users.noreply.github.com>
2024-04-27 22:18:05 +0000

    Implemented a feature to display a random dad joke on the blog homepage.

--- django_project/views.py
+++ django_project/views.py
@@ -1,17 +1,21 @@
 import requests
 from django.shortcuts import render
 
+
 def home(request):
   # USING APIS => Example 1
-  response = requests.get('https://api.github.com/events')
-  data = response.json()
-  result = data[0]["repo"]
+  reponse1 = requests.get('https://dog.ceo/api/breeds/image/random')
+  data1 = reponse1.json()
+  result1 = data1["message"]
 
-  # Example 2
-  reponse2 = requests.get('https://dog.ceo/api/breeds/image/random')
-  data2 = reponse2.json()
-  result2 = data2["message"]
+  url = 'https://icanhazdadjoke.com/'
+  headers = {'Accept': 'application/json'}
 
+  response2 = requests.get(url, headers=headers)
+  data2 = response2.json()
+  result2 = data2["joke"]
 
-  
-  return render(request, 'templates/index.html', {'result': result, 'result2': result2})
+  return render(request, 'templates/base.html', {
+      'result1': result1,
+      'result2': result2
+  })
